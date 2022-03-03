790308e432a857d104c85b4d8eb461d6c01e91ac
Michael <michael.abid@outlook.Fr>
2022-03-03 21:27:23 +0100

    Debut des fake Account pour le proto

--- Assets/Scriptables/User/Nft.cs
+++ Assets/Scriptables/User/Nft.cs
@@ -2,6 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
+[CreateAssetMenu(fileName = "NFT", menuName = "Create an NFT", order = 2)]
 public class Nft : ScriptableObject
 {
     public int nft_id;
--- Assets/Scriptables/User/Wallet.cs
+++ Assets/Scriptables/User/Wallet.cs
@@ -2,6 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
+[CreateAssetMenu(fileName = "Wallet", menuName = "Create a Wallet", order = 3)]
 public class Wallet : ScriptableObject
 {
     public int wallet_id;
