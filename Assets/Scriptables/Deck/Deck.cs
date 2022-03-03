ab96386ada8d1601746a57288f4fa1514b769584
MichaelAbid <91900412+MichaelAbid@users.noreply.github.com>
2022-03-03 16:58:47 +0100

    merge

--- Assets/Scriptables/Deck/Deck.cs
+++ Assets/Scriptables/Deck/Deck.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[CreateAssetMenu(fileName = "Deck_AI", menuName = "Create a Deck For Ai", order = 1)]
 public class Deck : ScriptableObject
 {
     public List<Card> listOfCard;
