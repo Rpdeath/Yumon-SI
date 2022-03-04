fed1cde9cc302268f7d1039d6b5ee9aff6a1ff72
MichaelAbid <91900412+MichaelAbid@users.noreply.github.com>
2022-03-04 15:39:22 +0100

    PAssive

--- Assets/Scriptables/Properties/Passive.cs
+++ Assets/Scriptables/Properties/Passive.cs
@@ -10,7 +10,20 @@ public interface IPassive
 
 public class Passive : MonoBehaviour, IPassive
 {
-   
+
+    float speedRatio = 1f;
+    Card generator;
+    Deck Ally;
+    Deck Ennemy;
+    private void Start()
+    {
+        GetComponent<HypeGenerator>().boost = speedRatio;
+    }
+
+    public void UpdatePassiveOnNewCard()
+    {
+
+    }
 
     public void ProcPassive(Card generator,Deck Ally, Deck Ennemy)
     {
