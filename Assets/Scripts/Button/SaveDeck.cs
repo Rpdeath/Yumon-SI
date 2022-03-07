b576c991405dc28a0a8cbf3325c51114336f1a3f
MichaelAbid <91900412+MichaelAbid@users.noreply.github.com>
2022-03-07 14:27:05 +0100

    collections

--- Assets/Scripts/Button/SaveDeck.cs
+++ Assets/Scripts/Button/SaveDeck.cs
@@ -2,13 +2,26 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class SaveDeck : MonoBehaviour
 {
    
     public void onClick()
+    {
+     
+        CreateCollection creator =  GameObject.FindObjectOfType<CreateCollection>();
+        if (creator.currentCardsonDeck < creator.maxCardOnDeck) return;
+        Deck dc = (Deck)ScriptableObject.CreateInstance("Deck");
+        foreach (Card card in creator.listofSCards)
         {
-            
+            dc.listOfCard.Add(card);
         }
+
+        GameInstance.instance.userData.deck = dc;
+
+
+
+    }
 
 
 }
--- Assets/Scripts/Collection/CreateCollection.cs
+++ Assets/Scripts/Collection/CreateCollection.cs
@@ -8,41 +8,60 @@ public class CreateCollection : MonoBehaviour
     GameInstance gi;
     public Object prefab;
     public List<GameObject> listofCards;
-    public int maxCardOnDeck=10;
-    public int currentCardsonDeck=0;
+    public List<Card> listofSCards;
+    public int maxCardOnDeck = 10;
+    public int currentCardsonDeck = 0;
     public Text count;
 
-    
+    public User usData;
+
+    // {"id":"83","name":"Bob Lennon","pathOfImage":"bob_lennon","listOfTags":[2],"rarity":1}
     private void Start()
     {
-        gi = Object.FindObjectOfType<GameInstance>();
-        int perline = 10;
-        Vector3 pos = transform.position- (Vector3.right*((perline/2)+3));
-        
+
+        gi = GameInstance.instance;
+        if (gi == null)
+        {
+            gi = new GameInstance();
+            gi.userData = usData;
+        }
+        int perline = 6;
+
+        Vector3 pos = transform.position - (Vector3.right * ((perline / 2) + 3));
+
         int i = 0;
-        //Debug.Log(JsonUtility.ToJson(gi.userData));
+        Debug.Log(JsonUtility.ToJson(gi.userData));
         foreach (Nft nft in gi.userData.nfts)
         {
             if (JsonUtility.ToJson(nft) != "")
             {
-                
+
                 Card card = (Card)ScriptableObject.CreateInstance("Card");
                 JsonUtility.FromJsonOverwrite(nft.nft_data, card);
-                Debug.Log(JsonUtility.ToJson(card));
-                GameObject clone = (GameObject) Instantiate(prefab,transform);
+                //Debug.Log(JsonUtility.ToJson(card));
+                GameObject clone = (GameObject)Instantiate(prefab, transform);
                 clone.GetComponent<CarDataManager>().CardData = card;
                 clone.GetComponent<CardCollectionSelectOnClick>().collectionCreator = this;
-                clone.transform.position = pos+(Vector3.right*1.5f);
+
+                clone.transform.position = pos + (Vector3.right * 3f);
+
                 pos = clone.transform.position;
                 listofCards.Add(clone);
+                listofSCards.Add(card);
                 i++;
             }
             if (i >= perline)
             {
                 i = 0;
                 pos.x = transform.position.x - ((perline / 2) + 3);
-                pos.y -= 2f;
+
+                pos.y -= 3f;
+
             }
         }
+
+        //transform.position = new Vector3(0, 0, 0);
+
+
     }
 }
