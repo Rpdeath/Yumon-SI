845bb64028cd81fa8f8b9285589cc64264ffa990
MichaelAbid <91900412+MichaelAbid@users.noreply.github.com>
2022-03-09 13:16:13 +0100

    multi part1

--- Assets/Scripts/General/GameInstance.cs
+++ Assets/Scripts/General/GameInstance.cs
@@ -220,4 +220,94 @@ public class GameInstance : MonoBehaviour
         }
     }
 
+
+    public void UpdateOponentData(string jsonData)
+    {
+
+        if (jsonData[13] == '0')
+        {
+
+            User a = (User)ScriptableObject.CreateInstance("User");
+            string b = jsonData.Remove(0, 25);
+            b = b.Remove(b.Length - 1, 1);
+            //Debug.Log(b);
+            JsonUtility.FromJsonOverwrite(b, a);
+            string c = (b.Substring(b.IndexOf("\"nfts\""), b.Length - 1 - b.IndexOf("\"nfts\""))).Remove(0, 8);
+            c = c.Remove(c.Length - 1, 1);
+            //Debug.Log(c);
+            userData = a;
+            string d = c.Replace("},{", "}|{");
+            foreach (var item in d.Split('|'))
+            {
+                Nft nft = (Nft)ScriptableObject.CreateInstance("Nft");
+                JsonUtility.FromJsonOverwrite(item, nft);
+                userData.nfts.Add(nft);
+            }
+
+            string w = (b.Substring(b.IndexOf("\"wallets\"") + 3, b.IndexOf("\"nfts\"") - 4 - b.IndexOf("\"wallets\""))).Remove(0, 8);
+            w = w.Remove(w.Length - 1, 1);
+            Debug.Log(w);
+
+            string x = w.Replace("},{", "}|{");
+            foreach (var item in x.Split('|'))
+            {
+                //Debug.Log(item);
+                Wallet wallet = (Wallet)ScriptableObject.CreateInstance("Wallet");
+                JsonUtility.FromJsonOverwrite(item, wallet);
+                userData.wallets.Add(wallet);
+            }
+
+            Deck deck = (Deck)ScriptableObject.CreateInstance("Deck");
+
+            userData.deck = deck;
+            deck.listOfCard = new List<Card>();
+
+            string de = (b.Substring(b.IndexOf("\"deck\"") - 1, b.IndexOf("\"wallets\"") + 1 - b.IndexOf("\"deck\""))).Remove(0, 8);
+            de = de.Remove(de.Length - 1, 1);
+            Debug.Log(de);
+
+            string xe = de.Replace("},{", "}|{");
+            foreach (var item in xe.Split('|'))
+            {
+
+                //Debug.Log(item);
+                string xee = (item.Substring(item.IndexOf("\"deck_data\"") + 1, item.IndexOf(",\"deck_owner\"") - 1 - item.IndexOf("\"deck_data\""))).Remove(0, 11);
+                string xei = (item.Substring(item.IndexOf("\"deck_id\"") + 1, item.IndexOf(",\"deck_data\"") - 2 - item.IndexOf("\"deck_id\""))).Remove(0, 10);
+                deck.deck_id = int.Parse(xei);
+                SerializedDeck sDeck = new SerializedDeck();
+                xee = (xee.Remove(xee.Length - 1, 1)).Remove(0, 1).Replace("\\\"", "\"");
+
+                JsonUtility.FromJsonOverwrite(xee, sDeck);
+                List<Card> lCard = new List<Card>();
+                foreach (Nft nft in GameInstance.instance.userData.nfts)
+                {
+                    if (JsonUtility.ToJson(nft) != "")
+                    {
+                        Card card = (Card)ScriptableObject.CreateInstance("Card");
+                        JsonUtility.FromJsonOverwrite(nft.nft_data, card);
+
+                        card.propertie = (Properties)GameInstance.instance.ScriptRef.AssetList_script[GameInstance.instance.ScriptRef.AssetList_id.IndexOf(card.pathOfImage)];
+                        lCard.Add(card);
+
+                    }
+                }
+                foreach (int i in sDeck.listOfCardId)
+                {
+                    foreach (Card card in lCard)
+                    {
+                        if (card.id == i)
+                        {
+                            deck.listOfCard.Add(card);
+                        }
+                    }
+                }
+
+
+            }
+
+
+
+        }
+    }
+
 }
