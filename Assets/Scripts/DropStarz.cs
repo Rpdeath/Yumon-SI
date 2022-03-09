1ae5e5ec4d6ca91633993874b641e61860dd6154
MichaelAbid <91900412+MichaelAbid@users.noreply.github.com>
2022-03-09 10:27:36 +0100

    Revert "merge"
    This reverts commit 52e7f2cd996e1e1af27d27f8d5cdbe11382ba18e, reversing
    changes made to b21e1257db11d855cd66c9ccda8f9f1b1a3c9c4b.

--- Assets/Scripts/DropStarz.cs
+++ Assets/Scripts/DropStarz.cs
@@ -4,10 +4,6 @@ using UnityEngine;
 
 public class DropStarz : MonoBehaviour, IReceive
 {
-    [Header ("Ui")]
-    public ActifButtonUi uiActif;
-
-
     public ScriptReferencement ScriptRef;
     public GameObject actualStarz;
     public Transform spawnPoint;
@@ -20,6 +16,7 @@ public class DropStarz : MonoBehaviour, IReceive
         
         if (actualStarz == null && allowDrop)
         {
+            Debug.Log("Drop Starz");
             Card card = obj.GetComponent<CarDataManager>().CardData;
             GameObject gobj = null;
             if (assetRef.AssetList_id.IndexOf(card.pathOfImage) != -1) gobj = assetRef.AssetList_model[assetRef.AssetList_id.IndexOf(card.pathOfImage)] as GameObject;
@@ -39,7 +36,6 @@ public class DropStarz : MonoBehaviour, IReceive
     {
         actualStarz = Instantiate(objToSpawn, spawnPoint.position, spawnPoint.rotation);
         actualStarz.GetComponentInChildren<HypeGenerator>().timeToCompletion = card.propertie.speed;
-        actualStarz.GetComponentInChildren<StarzActifSysteme>().selfActifUi = uiActif;
         GameInstance.instance.actualGameInfo.manaPlayer1 -= card.propertie.cost;
         actualStarz.GetComponentInChildren<StarzData>().data = card;
         actualStarz.GetComponentInChildren<StarzData>().pos = pillarId;
